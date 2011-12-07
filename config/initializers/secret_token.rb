28619da7a1773c63993348045889d934821187aa
Greg Funk <gfunk@apangea.com>
2011-12-07 16:49:27 -0500

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GregsFirstApp::Application.config.secret_token = '9df7d522be9d60034dc1c23e3fc6caaeced54de52341294ad951896ceccb328b4ab0eaaff88fc9fab3de07f664718863fa273d3e1163e1c9586e6034b4c5fe4a'
